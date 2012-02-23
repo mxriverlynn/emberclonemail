574f7866f4d1065aa72bf975f22fd55c44d349ed
Derick Bailey <derickbailey@gmail.com>
2012-02-22 21:05:26 -0600

    renaming files... thinking about sub-states and higher level aggregate state. not sure this is going to work out

--- public/javascripts/emberclonemail.email.js
+++ public/javascripts/emberclonemail.email.js
@@ -1,69 +1,7 @@
-EmberCloneMail.Email = Ember.Object.extend({});
+EmberCloneMail.email = (function(Ember, ECM){
+  var email =  Ember.StateManager.create({
+    states: {}
+  });
 
-EmberCloneMail.EmailListView = Em.View.extend({
-  tagName: "ul",
-  classNames: ["email-list", "email-view"],
-  templateName: "email-list-view"
-});
-
-EmberCloneMail.EmailPreview = Em.View.extend(Ember.TargetActionSupport, {
-  tagName: "li",
-  templateName: "email-preview",
-
-  click: function(e){
-    this.triggerAction();
-  }
-});
-
-EmberCloneMail.EmailView = Em.View.extend({
-  tagName: "ul",
-  classNames: ["email-list"],
-  templateName: "email-view"
-});
-
-EmberCloneMail.emailStates = Ember.StateManager.create({
-  rootElement: "#main",
-
-  showInbox: Ember.ViewState.create({
-    view: EmberCloneMail.EmailListView.create()
-  }),
-
-  showEmail: Ember.ViewState.create({
-    view: EmberCloneMail.EmailView.create()
-  })
-
-});
-
-EmberCloneMail.emailController = Ember.ArrayController.create({
-  content: [],
-
-  selectedEmail: null,
-
-  loadEmail: function(){
-    var that = this;
-    $.ajax({
-      url: "/email",
-      dataType: "json",
-
-      success: function(emailList){
-        var email = emailList.map(function(emailData){
-          return EmberCloneMail.Email.create(emailData);
-        });
-        that.set("content", email);
-      }
-    });
-  },
-
-  showInbox: function(){
-    EmberCloneMail.emailController.loadEmail();
-    EmberCloneMail.emailStates.goToState("showInbox");
-  },
-  
-  showEmail: function(view){
-    var email = view.get("content");
-    this.set("selectedEmail", email);
-    EmberCloneMail.emailStates.goToState("showEmail");
-  }
-});
-
-EmberCloneMail.emailController.showInbox();
+  return email;
+})(Ember, EmberCloneMail);
